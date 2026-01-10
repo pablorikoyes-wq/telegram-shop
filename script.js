bf8ea26bdcf3e52394fc5f172efe5705274fe927
Otabek <pablorikoyes@gmail.com>
2026-01-11 00:41:23 +0300

    increase bottom nav size for mobile

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 const tg = window.Telegram.WebApp;
 tg.ready();
+
+// переключение активной кнопки
+document.querySelectorAll('.nav-item').forEach(btn => {
+  btn.addEventListener('click', () => {
+    document.querySelectorAll('.nav-item').forEach(b => b.classList.remove('active'));
+    btn.classList.add('active');
+  });
+});
